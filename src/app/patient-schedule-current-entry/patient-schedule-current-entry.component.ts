a0e7a4628b326c3d6c1350b6df53c3f9815f0ca7
Ahmad Abdelghany <ahmad7cs@gmail.com>
2024-04-06 13:51:01 +0200

    Added background animation to current patient in schedule

--- src/app/patient-schedule-current-entry/patient-schedule-current-entry.component.ts
+++ src/app/patient-schedule-current-entry/patient-schedule-current-entry.component.ts
@@ -6,7 +6,32 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [MatIconModule],
   templateUrl: './patient-schedule-current-entry.component.html',
-  styles: ``
+  styles: `
+  .elems-container {
+    background: linear-gradient(270deg, #8EE0FB, #FF79E1, #F7E3AF);
+    background-size: 600% 600%;
+
+    -webkit-animation: AnimationName 2s ease infinite;
+    -moz-animation: AnimationName 2s ease infinite;
+    animation: AnimationName 9s ease infinite;
+  }
+
+  @-webkit-keyframes AnimationName {
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
+  @-moz-keyframes AnimationName {
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
+  @keyframes AnimationName {
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
+  `
 })
 export class PatientScheduleCurrentEntryComponent {
 
